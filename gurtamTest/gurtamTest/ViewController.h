731f38923da05eedcd6774176158bdfe1a18b07c
Alexey Gurbo <a.gurbo@softteco.com>
2015-09-23 21:21:59 +0300

    initial commit

---
+++ gurtamTest/gurtamTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  gurtamTest
+//
+//  Created by Hurbo Aliaksei on 9/23/15.
+//  Copyright © 2015 Aliaksei Hurbo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ gurtamTest/gurtamTest/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  gurtamTest
+//
+//  Created by Hurbo Aliaksei on 9/23/15.
+//  Copyright © 2015 Aliaksei Hurbo. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
